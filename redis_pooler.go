315539f4c2df9b595132589fbd6aa58025e2cb7f
iamharvey <39893271+iamharvey@users.noreply.github.com>
2018-12-05 13:38:37 +0800

    Make it a package

--- redis_pooler.go
+++ redis_pooler.go
@@ -23,11 +23,16 @@ func newPool() *redis.Pool {
 	mode := viper.GetString("Mode")
 	host := viper.GetString(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "host"))
 	pass := viper.GetString(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "pass"))
+	maxIdle := viper.GetInt(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "maxIdle"))
+	maxActive := viper.GetInt(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "maxActive"))
+	maxConnLifetime := viper.GetDuration(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "maxConnLifetime"))
+	idleTimeout := viper.GetDuration(fmt.Sprintf("%v.%v.%v", mode, "redisDB", "idleTimeout"))
+
 	return &redis.Pool{
-		MaxIdle:   5,
-		MaxActive: 12000, // max number of connections
-		MaxConnLifetime: 3 * time.Minute,
-		IdleTimeout: 200 * time.Second,
+		MaxIdle:   maxIdle,
+		MaxActive: maxActive, // max number of connections
+		MaxConnLifetime: maxConnLifetime * time.Second,
+		IdleTimeout: idleTimeout * time.Second,
 		Dial: func() (redis.Conn, error) {
 			c, err := redis.Dial("tcp", host + ":6379", redis.DialPassword(pass))
 			if err != nil {
@@ -39,7 +44,6 @@ func newPool() *redis.Pool {
 }
 
 func removeHooks() {
-
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt)
 	signal.Notify(c, syscall.SIGTERM)
